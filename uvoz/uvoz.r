519789b3a45e8720be60e537e8e37f57833ebbe0
melamalej <mela.malej@gmail.com>
2019-01-03 13:44:45 +0100

    Vse tabele so urejene.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -6,7 +6,7 @@ require(readr)
 
 library(reshape2)
 
-#Regije 
+#Regije (tabela1)
 tabela1 <- read_csv2("št.podjetij_regije2008.csv", skip = 4, n_max = 12, col_names = c("Regija", 2008:2016),
                      locale = locale(encoding = "Windows-1250"))
 tabela1 <- tabela1 %>% melt(tabela1, value.name = "Število podjetij", id.vars = "Regija", measure.vars = names(tabela1)[-1],
@@ -99,7 +99,7 @@ Panoge2008 <- bind_cols(panoge1, panoge3[3])
 #tabela3
 p2 <- read_csv2("št.pod_panoge2007.csv", skip = 4, n_max = 9, col_names = c("Panoga", 2008:2016),
                 locale = locale(encoding = "Windows-1250"))
-panoge2 <- p2 %>% melt(p2, value.name = "Število oseb", id.vars = "Panoga", measure.vars = names(p2)[-1],
+panoge2 <- p2 %>% melt(p2, value.name = "Število podjetij", id.vars = "Panoga", measure.vars = names(p2)[-1],
                             variable.name = "Leto")
 panoge2$Leto <- parse_number(panoge2$Leto)
 panoge2$Panoga[panoge2$Panoga == "C RUDARSTVO"] <- "Rudarstvo"
@@ -112,7 +112,57 @@ panoge2$Panoga[panoge2$Panoga == "I PROMET, SKLADIŠČENJE IN ZVEZE"] <- "Promet
 panoge2$Panoga[panoge2$Panoga == "J FINANČNO POSREDNIŠTVO"] <- "Finančno posredništvo"
 panoge2$Panoga[panoge2$Panoga == "K POSLOVANJE Z NEPREMIČNINAMI, NAJEM IN POSLOVNE STORITVE"] <- "Poslovanje z nepremičninami, najem in poslovne storitve"
 
+p4 <- read_csv2("št.oseb_panoge2007.csv", skip = 4, n_max = 9, col_names = c("Panoga", 2008:2016),
+                locale = locale(encoding = "Windows-1250"))
+panoge4 <- p4 %>% melt(p4, value.name = "Število oseb", id.vars = "Panoga", measure.vars = names(p4)[-1],
+                       variable.name = "Leto")
+panoge4$Leto <- parse_number(panoge4$Leto)
+panoge4$Panoga[panoge4$Panoga == "C RUDARSTVO"] <- "Rudarstvo"
+panoge4$Panoga[panoge4$Panoga == "D PREDELOVALNE DEJAVNOSTI"] <- "Predelovalne dejavnosti"
+panoge4$Panoga[panoge4$Panoga == "E OSKRBA Z ELEKTRIČNO ENERGIJO, PLINOM IN VODO"] <- "Oskrba z električno energijo, plinom in vodo"
+panoge4$Panoga[panoge4$Panoga == "F GRADBENIŠTVO"] <- "Gradbeništvo"
+panoge4$Panoga[panoge4$Panoga == "G TRGOVINA, POPRAVILA MOTORNIH VOZIL IN IZDELKOV ŠIROKE PORABE"] <- "Trgovina, popravila motornih vozil in izdelkov široke porabe"
+panoge4$Panoga[panoge4$Panoga == "H GOSTINSTVO"] <- "Gostinstvo"
+panoge4$Panoga[panoge4$Panoga == "I PROMET, SKLADIŠČENJE IN ZVEZE"] <- "Promet, skladiščenje in zveze"
+panoge4$Panoga[panoge4$Panoga == "J FINANČNO POSREDNIŠTVO"] <- "Finančno posredništvo"
+panoge4$Panoga[panoge4$Panoga == "K POSLOVANJE Z NEPREMIČNINAMI, NAJEM IN POSLOVNE STORITVE"] <- "Poslovanje z nepremičninami, najem in poslovne storitve"
 
+Panoge2007 <- bind_cols(panoge2, panoge4[3]) 
 
+#tabela4
+Panoge2 <- c(rep(c("Rudarstvo","Predelovalne dejavnosti", 
+                 "Oskrba z električno energijo, plinom in paro",
+                 "Oskrba z vodo, ravnanje z odplakami in odpadki, saniranje okolja",
+                 "Gradbeništvo","Trgovina,vzdrževanje in popravila motornih vozil",
+                 "Promet in skladiščenje", "Gostinostvo",
+                 "Informacijske in komunikacijske dejavnosti",
+                 "Poslovanje z nepremičninami", 
+                 "Strokovne, znanstvene in tehnične dejavnosti", 
+                 "Druge raznovrstne poslovne dejavnosti"), 13))
+Leta2 <- c(rep(2005:2017, each = 12))            
+            
+p5 <- read_csv2("letno.csv", skip = 2, n_max = 13, locale = locale(encoding = "Windows-1250"))
+prihodki = p5[2:13,2:14]
+presežek = p5[2:13,15:27]
+zaposleni = p5[2:13,28:40]
+prihodki <- data.frame(Prihodek=unlist(prihodki, use.names = FALSE))
+presežek <- data.frame(Presežek=unlist(presežek, use.names = FALSE))
+zaposleni <- data.frame(Zaposleni=unlist(zaposleni, use.names = FALSE))
 
+Panoge <- cbind(Panoge2,Leta2, prihodki, presežek, zaposleni)
+Panoge <- as.data.frame(Panoge)
+colnames(Panoge) <- c("Panoga", "Leto", "Prihodki od prodaje (1000 EUR)",
+                       "Bruto poslovni presežek (1000 EUR)", "Število zaposlenih")
+
+#tabela5
+link <- "https://en.wikipedia.org/wiki/Economy_of_Slovenia"
+stran <- html_session(link) %>% read_html()
+bdp <- stran %>% html_nodes(xpath="//table[@class='wikitable']") %>%.[[1]]%>% html_table()
+bdp <- data.frame(Leto = 2005:2017, BDP = t(bdp[3, 5:17]), row.names = NULL)
+bdp$X3 <- gsub("\\%", "", bdp$X3)
+bdp$X3 <- parse_number(bdp$X3)
+names(bdp) <- c("Leto", "Sprememba BDP")
+bdp$`Sprememba BDP`[5] <- -7.8
+bdp$`Sprememba BDP`[8] <- -2.7
+bdp$`Sprememba BDP`[9] <- -1.1
 
