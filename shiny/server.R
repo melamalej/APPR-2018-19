6c396a2a29eb4ef95be3032849d6e80510aaed8f
melamalej <mela.malej@gmail.com>
2019-02-08 20:32:13 +0100

    Poskus popravka, a še vedno ne deluje.

--- shiny/server.R
+++ shiny/server.R
@@ -6,16 +6,18 @@ shinyServer(function(input, output){
     
     spr <- input$spremenljivka
     data <- Panoge %>% filter(Panoga == input$panoga) %>% select(input$spremenljivka)
-                              
-    g <- ggplot(data, aes(x = "Leto", y = spr)) + 
-      geom_point() 
     
-    g + xlim(2005, 2023) +
-      geom_smooth(method = 'lm', fullrange = TRUE, se = FALSE) +
-      xlab("Leto") + ylab("Spremenljivka") +
-      scale_x_discrete()
+    fit <- lm(data, spr ~ Leto)
+    l <- data.frame(Leto=seq(2005, 2023))
+    predict(fit, l)
+    napoved <- l %>% mutate(spr=predict(fit, .))
+    
+    lin1 <- ggplot(data, aes(x=Leto, y=spr)) +
+      geom_line() +
+      geom_point(data=napoved, aes(x=Leto, y=spr), color="blue", size=3) + 
+      geom_smooth(method = 'lm', se = FALSE) +
+      xlab("Leto") + ylab("Spremenljivka") 
     
     })
 })
-
 
