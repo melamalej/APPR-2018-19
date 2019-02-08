f6983b0609f8f4bf53e2f664e5cc5513fa16dcc6
melamalej <mela.malej@gmail.com>
2019-02-08 17:05:07 +0100

    Shiny - ne izriše pravega grafa.

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,21 @@
 library(shiny)
 
-shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var="stevilo.druzin") %>%
-      rename(`Občina`=obcina)
-  })
+shinyServer(function(input, output){
   
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x=naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
-  })
+  output$panoge <- renderPlot({
+    
+    spr <- input$spremenljivka
+    data <- Panoge %>% filter(Panoga == input$panoga) %>% select(input$spremenljivka)
+                              
+    g <- ggplot(data, aes(x = "Leto", y = spr)) + 
+      geom_point() 
+    
+    g + xlim(2005, 2023) +
+      geom_smooth(method = 'lm', fullrange = TRUE, se = FALSE) +
+      xlab("Leto") + ylab("Spremenljivka") +
+      scale_x_discrete()
+    
+    })
 })
+
+
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,29 @@
 library(shiny)
 
+#shinyUI(
+  #ui <- fluidPage(
+    #titlePanel("A"),
+    #tabsetPanel(
+      #tabPanel("Leto",
+               #sliderInput(inputId="leto",label="Leto",min=2005,max=2017,value=2005,sep=""),
+               #plotOutput("panoge"))
+    #)
+  #)
+#)
+
 shinyUI(fluidPage(
   
-  titlePanel("Slovenske občine"),
-  
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
+  titlePanel("Analiza posamezne panoge"),
+   
+  sidebarLayout(mainPanel(), sidebarPanel('Na spodnjem grafu lahko vidimo predikcijo na podlagi linearne regresije.')),
+        
+  selectInput(inputId = "panoga", label = "Izberi panogo:", choices = panoge),
+    
+  selectInput(inputId = "spremenljivka", label = "Izberi spremenljivko:",
+              choices = c("Prihodki_od_prodaje_v_tisoč_EUR", "Bruto_poslovni_presežek_v_tisoč_EUR", 
+                        "Število_zaposlenih")),
+    
+  plotOutput("panoge")
+    
 ))
+  
